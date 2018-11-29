97d8595246f26db70cc7f7ca851b2da9a1caabf2
Alexandr Dikov <alxdeex@gmail.com>
2018-11-29 22:05:39 +0300

    edit migrations

--- database/migrations/2018_11_26_194455_create_transactions_table.php
+++ database/migrations/2018_11_26_194455_create_transactions_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateTransactionsTable extends Migration
 {
@@ -15,12 +16,14 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->integer('account_id');
             $table->integer('category_id');
             $table->integer('user_id');
             $table->decimal('sum_amount');
             $table->integer('currency')->default('643');
+            $table->timestamp('date')->default(DB::raw('CURRENT_TIMESTAMP'));
 
             $table->index(['user_id', 'account_id'], 'user_account_index');
         });
