9318166a21ee81950107895c7fccd09a6ef7e71b
Alexandr Dikov <alxdeex@gmail.com>
2018-11-29 23:33:18 +0300

    add transaction controller

--- app/Account.php
+++ app/Account.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Account
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Account newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Account newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Account query()
+ * @mixin \Eloquent
+ */
 class Account extends Model
 {
+    protected $fillable = ['user_id'];
     //
 }
