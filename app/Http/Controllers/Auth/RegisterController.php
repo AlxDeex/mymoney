ae5534e06768b07c084a8de2fa14d3db44e3cb9d
Alexandr Dikov <alxdeex@gmail.com>
2018-11-29 23:32:49 +0300

    add creation Account with register user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Account;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Category as Category;
 
 class RegisterController extends Controller
 {
@@ -28,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/home/spend';
 
     /**
      * Create a new controller instance.
@@ -63,10 +65,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        Account::create([
+            'user_id' => $user->id,
+        ]);
+
+        return $user;
     }
 }
